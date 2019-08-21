c8f0e2aa6116cf452dc8ab296e6796346faa34cf
Mara Bos <m-ou.se@m-ou.se>
2019-08-21 20:33:29 +0200

    Use a cortex-m Mutex to avoid unsafe code.
    This doesn't change the size of the HSTDOUT and HSTDERR objects, as both
    cortex-m's Mutex and core's Cell don't add any weight.

--- src/export.rs
+++ src/export.rs
@@ -1,51 +1,46 @@
 //! IMPLEMENTATION DETAILS USED BY MACROS
 
+use core::cell::Cell;
 use core::fmt::{self, Write};
 
-use cortex_m::interrupt;
+use cortex_m::interrupt::{self, Mutex};
 
 use crate::hio::{self, HostStream};
 
-static mut HSTDOUT: Option<HostStream> = None;
+static HSTDOUT: Mutex<Cell<Option<HostStream>>> = Mutex::new(Cell::new(None));
+static HSTDERR: Mutex<Cell<Option<HostStream>>> = Mutex::new(Cell::new(None));
 
 pub fn hstdout_str(s: &str) -> Result<(), ()> {
-    interrupt::free(|_| unsafe {
-        if HSTDOUT.is_none() {
-            HSTDOUT = Some(hio::hstdout()?);
-        }
-
-        HSTDOUT.as_mut().unwrap().write_str(s).map_err(drop)
-    })
+    with_lazy_init(&HSTDOUT, hio::hstdout, |x| x.write_str(s))
 }
 
 pub fn hstdout_fmt(args: fmt::Arguments) -> Result<(), ()> {
-    interrupt::free(|_| unsafe {
-        if HSTDOUT.is_none() {
-            HSTDOUT = Some(hio::hstdout()?);
-        }
-
-        HSTDOUT.as_mut().unwrap().write_fmt(args).map_err(drop)
-    })
+    with_lazy_init(&HSTDOUT, hio::hstdout, |x| x.write_fmt(args))
 }
 
-static mut HSTDERR: Option<HostStream> = None;
-
 pub fn hstderr_str(s: &str) -> Result<(), ()> {
-    interrupt::free(|_| unsafe {
-        if HSTDERR.is_none() {
-            HSTDERR = Some(hio::hstderr()?);
-        }
-
-        HSTDERR.as_mut().unwrap().write_str(s).map_err(drop)
-    })
+    with_lazy_init(&HSTDERR, hio::hstderr, |x| x.write_str(s))
 }
 
 pub fn hstderr_fmt(args: fmt::Arguments) -> Result<(), ()> {
-    interrupt::free(|_| unsafe {
-        if HSTDERR.is_none() {
-            HSTDERR = Some(hio::hstderr()?);
-        }
+    with_lazy_init(&HSTDERR, hio::hstderr, |x| x.write_fmt(args))
+}
 
-        HSTDERR.as_mut().unwrap().write_fmt(args).map_err(drop)
+fn with_lazy_init(
+    mutex: &Mutex<Cell<Option<HostStream>>>,
+    init: impl Fn() -> Result<HostStream, ()>,
+    f: impl Fn(&mut HostStream) -> Result<(), fmt::Error>,
+) -> Result<(), ()> {
+    interrupt::free(|cs| {
+        let cell = mutex.borrow(cs);
+        let mut stream = match cell.get() {
+            Some(s) => s,
+            None => {
+                let s = init()?;
+                cell.set(Some(s));
+                s
+            }
+        };
+        f(&mut stream).map_err(drop)
     })
 }
